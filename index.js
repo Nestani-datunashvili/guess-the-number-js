f142a2894f7856933b3136f75ed293ff13abf6d6
Nestani Datunashvili <nestani.datunashvili@gmail.com>
2024-09-19 11:51:52 +0400

    next try

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
 let handleInput = (event) => {
   event.preventDefault();
 
   let input = document.getElementById("input");
-  let inputValue = document.etElementById("input").value;
+  let inputValue = document.getElementById("input").value;
   let resultDiv = document.getElementById("result");
 
   resultDiv.textContent = "";
 
   if (inputValue.trim() === "" || isNaN(inputValue)) {
     resultDiv.textContent = "Enter number please";
-  } else if (inputValue >= 10) {
-    resultDiv.textContent = "make higher";
+  } else if (inputValue > randomNumber) {
+    resultDiv.textContent = "Higher";
+  } else if (inputValue < randomNumber) {
+    resultDiv.textContent = "lower";
+  } else if (inputValue >= randomNumber) {
+    resultDiv.textContent = "Good Job!! ";
+    randomNumber = Math.floor(Math.random() * 100) + 1;
   }
   input.value = "";
 };
